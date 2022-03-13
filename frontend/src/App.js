866173c1f4bd78cba7e9c609f39c99c0fb77ac92
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-13 03:00:36 -0700

    removed unnecessary front end packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 const App = () => {
