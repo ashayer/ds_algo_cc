e02eff34f4d052b3b8a8bb53051b692c9f3fdd5f
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-13 21:29:23 -0700

    removed node_env from .env file

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,12 @@ import goalRoutes from "./routes/goalRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middleware/errorMiddle.js";
 import { connectDB } from "./config/db.js";
-import path, { dirname } from "path";
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const port = 5000;
+//! use cors
 
 connectDB();
 
 const app = express();
-dotenv.config({ path: 'config.env' });
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,16 +19,8 @@ app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("/", (req, res) => {
+  res.send("Works");
+});
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("not in production"));
-}
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
