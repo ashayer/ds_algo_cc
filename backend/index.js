e1cdfbe60d9b1251b5a8fc940f833dc01c6c3ef9
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-22 00:19:07 -0700

    updated port config in index of backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middleware/errorMiddle.js";
 import { connectDB } from "./config/db.js";
-import { URL } from 'url';
+import { URL } from "url";
 import path from "path";
 //! use cors
 
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 //initialize dotenv
 dotenv.config();
-
+const PORT = process.env.PORT || 5000;
 //middleware function that parses request with JSON payloads
 app.use(express.json());
 
@@ -27,14 +27,12 @@ app.use("/api/users", userRoutes);
 //uses errorHandler middleware
 app.use(errorHandler);
 
-const __dirname = new URL('.', import.meta.url).pathname;
+const __dirname = new URL(".", import.meta.url).pathname;
 
 // app.use(express.static('../frontend/build/'));
 
-
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', {root: path.join("../frontend", "/public/")});
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: path.join("../frontend", "/public/") });
 });
 
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
