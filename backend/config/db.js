2aca961cb17b98396124d0ca54d764ffa8ad1100
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-24 22:43:22 -0700

    replaced process.exit with throw error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 //mongoose connection to cloud mongo
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MONGODB CONNECTED: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MONGODB CONNECTED: ${conn.connection.host}`);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
