1048ae8a3b1025e1f6ff11ae10b37beda5d55263
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-25 22:21:17 -0700

    added React Route to /learn

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,10 @@ import { registerUser, loginUser, updatePoints, getPoints } from "../controllers
 import protect from "../middleware/authMiddle.js";
 const router = express.Router();
 
-
-//the various routes the users has options for 
+//the various routes the users has options for
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.patch('/user/:id', updatePoints); //!does not properly authenticate so cannot use protect middleware
-router.get('/user/:id', getPoints);
+router.patch("/user/:id", updatePoints); //!does not properly authenticate so cannot use protect middleware
+router.get("/user/:id", getPoints);
 
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { Container } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
+import Learn from "./components/Learn/learn";
 
 const App = () => {
   const user = JSON.parse(sessionStorage.getItem("user"));
 
   return (
     <BrowserRouter>
-      <Container maxWidth="xl">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={!user ? <Register /> : <Home />} />
-          <Route path="/login" element={!user ? <Login /> : <Home />} />
-        </Routes>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={!user ? <Register /> : <Home />} />
+        <Route path="/login" element={!user ? <Login /> : <Home />} />
+        <Route path="/learn" element={<Learn />} />
+      </Routes>
     </BrowserRouter>
   );
 };
