51c0980826e7f15436c70bdd6eae67b7aa7a3400
Aran Hayer <ashayer@users.noreply.github.com>
2022-03-28 01:38:30 -0700

    updated home page to show instead show dashboard of site features

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect } from "react";
-import { Grid, Button } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Typography,
+  Card,
+  CardContent,
+  CardActionArea,
+  Box,
+  Container,
+} from "@mui/material";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
@@ -16,12 +25,143 @@ const Home = () => {
   }, [user, localUser, navigate]);
 
   return (
-    <>
+    <Container maxWidth="xl">
       <Navbar page="Home" />
-      <Grid container>
-        <Button>To Game</Button>
+      <Grid container spacing={3} sx={{ marginTop: 2, textAlign: "center" }}>
+        <Grid item xs={6} md={6}>
+          <Paper elevation={24}>
+            <Typography variant="h3" sx={{ border: "1px solid blue" }}>
+              Game
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Card sx={{ minWidth: "48%", minHeight: "30vh", border: "1px solid red" }}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    *User name* - Stats
+                  </Typography>
+                  <Typography>
+                    Get lifetime user stats, points, highest streak, avg response time, lifetime
+                    questions right, lifetime questions wrong
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ minWidth: "48%", minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea sx={{ minHeight: "30vh" }}>
+                  <CardContent>
+                    <Typography variant="h1">Play</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <Paper elevation={24}>
+            <Typography variant="h3" sx={{ border: "1px solid blue" }}>
+              Extras
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <Paper elevation={24}>
+            <Typography variant="h3" sx={{ border: "1px solid blue" }}>
+              Sorting Algorithms
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <Paper elevation={24}>
+            <Typography variant="h3" sx={{ border: "1px solid blue" }}>
+              Data structures
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+              <Card sx={{ minHeight: "30vh", border: "1px solid red" }}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Box>
+          </Paper>
+        </Grid>
       </Grid>
-    </>
+    </Container>
   );
 };
 
