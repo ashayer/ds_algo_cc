50e7d4f6597ca4a12c3747dbf506564440a82a35
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-09 00:51:40 -0700

    added slider component to change array size and random generate button

--- frontend/src/components/SortingSandbox/SortingSandbox.jsx
+++ frontend/src/components/SortingSandbox/SortingSandbox.jsx
@@ -1,26 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { Box, Typography, Grid, Button } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Box,
+  Typography,
+  Grid,
+  Button,
+  AppBar,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Slider,
+} from "@mui/material";
 import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material/styles";
+import Navbar from "../Navbar/Navbar";
+
+const Line1 = "1 for (int i = 0; i < arr.len; i++)\n";
+const Line2 = "2   for (int j = i; j > 0; j--)\n";
+const Line3 = "3     if (arr[j] < arr[j-1])\n";
+const Line4 = "4       swap (arr[j],arr[j-1])\n";
+const Line5 = "5     else break;    ";
+
+const pseudoCodeStringArray = [Line1, Line2, Line3, Line4, Line5];
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
 const SortingSandbox = () => {
+  const [algorithm, setAlgorithm] = useState("");
+  const arraySize = useRef(15);
   const [arrayElements, setArrayElements] = useState([]);
   const [arrayMax, setArrayMax] = useState(1);
-  const [sortHistoryArray, setSortHistoryArray] = useState([
-    [
-      { value: 1, color: "blue" },
-      { value: 2, color: "blue" },
-      { value: 5, color: "blue" },
-      { value: 6, color: "blue" },
-      { value: 4, color: "blue" },
-    ],
-  ]);
+  const [sortHistoryArray, setSortHistoryArray] = useState([[{}]]);
   const [step, setStep] = useState(0);
   const createRandomArray = () => {
+    setStep(0);
     const tempArray = [];
-    for (let i = 0; i < 5; i += 1) {
+    for (let i = 0; i < arraySize.current; i += 1) {
       const elementObject = {
         value: Math.floor(Math.random() * (99 - 10) + 10),
         color: "blue",
@@ -92,10 +107,79 @@ const SortingSandbox = () => {
     createRandomArray();
   }, []);
 
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setAlgorithm(e.target.value);
+  };
+
+  const handleSliderChange = (e, value) => {
+    arraySize.current = value;
+    createRandomArray();
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{}}>
-        <Box sx={{ border: "1px solid black" }}>
+      <Navbar page="Sorting Algorithm Sandbox" />
+      <Box>
+        <AppBar
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            paddingLeft: "15px",
+            backgroundColor: "white",
+            mt: "2vh",
+          }}
+          position="static"
+        >
+          <FormControl sx={{ width: "15vw" }}>
+            <InputLabel id="demo-simple-select-label">Algorithm</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={algorithm}
+              label="Algorithm"
+              onChange={handleChange}
+              defaultValue=""
+            >
+              <MenuItem value={1}>Insertion Sort</MenuItem>
+              <MenuItem value={2}>Selection Sort</MenuItem>
+              <MenuItem value={3}>Merge Sort</MenuItem>
+              <MenuItem value={4}>Quick Sort</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            variant="contained"
+            onClick={() => {
+              createRandomArray();
+            }}
+          >
+            Generate New Random Array
+          </Button>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={15}
+            onChangeCommitted={(e, value) => {
+              handleSliderChange(e, value);
+            }}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={3}
+            max={15}
+            sx={{ width: "15vw" }}
+          />
+          <Button
+            variant="contained"
+            onClick={() => {
+              sortArrayInsertion();
+            }}
+          >
+            Sort
+          </Button>
+        </AppBar>
+        <Box sx={{ border: "1px solid black", mt: "2vh" }}>
           <Grid
             container
             sx={{
@@ -104,7 +188,7 @@ const SortingSandbox = () => {
             }}
           >
             {sortHistoryArray[step].map((element, idx) => (
-              <Grid item key={idx} sx={{ height: "75vh", width: "6vw" }}>
+              <Grid item key={idx} sx={{ height: "35vh", width: "6vw" }}>
                 <Box
                   sx={{
                     height: `${(element.value * 100) / arrayMax}%`,
@@ -117,7 +201,7 @@ const SortingSandbox = () => {
                     transition: "all 0.15s ease",
                   }}
                 >
-                  <Typography variant="h4" sx={{ width: "4vw" }}>
+                  <Typography variant="h4" sx={{ width: "6vw" }}>
                     {`${element.value}`}
                   </Typography>
                 </Box>
@@ -125,20 +209,31 @@ const SortingSandbox = () => {
             ))}
           </Grid>
         </Box>
-        <Button
-          variant="contained"
-          onClick={() => {
-            sortArrayInsertion();
-          }}
-        >
-          Sort Using Selection
-        </Button>
+
         <Button variant="contained" onClick={prevStep}>
           Prev Step
         </Button>
         <Button variant="contained" onClick={nextStep}>
           Next Step
         </Button>
+
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Grid item xs={12} md={6}>
+            <Typography
+              variant="h5"
+              style={{ whiteSpace: "break-spaces" }}
+              sx={{ textAlign: "left" }}
+            >
+              {pseudoCodeStringArray}
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
     </ThemeProvider>
   );
