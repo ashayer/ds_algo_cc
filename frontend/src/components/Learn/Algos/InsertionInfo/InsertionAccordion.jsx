20f943a9aa3c6fbd73bb80907f2685dede71342b
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-12 23:19:05 -0700

    add modal to insertion accordion and button to take quiz

--- frontend/src/components/Learn/Algos/InsertionInfo/InsertionAccordion.jsx
+++ frontend/src/components/Learn/Algos/InsertionInfo/InsertionAccordion.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Modal, Box } from "@mui/material";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import InsertionGeneral from "./InsertionGeneral";
@@ -13,13 +13,17 @@ import InsertionCode from "./InsertionCode";
 // eslint-disable-next-line no-unused-vars
 const InsertionAccordion = ({ tempSectionArray, setTempSectionArray, updateLocalUser }) => {
   const [currentSubSection, setCurrentSubSection] = useState("");
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const subsectionIndexRef = useRef(0);
   function handleAccordClick(name) {
     if (currentSubSection === name) setCurrentSubSection("");
     if (currentSubSection !== name) setCurrentSubSection(name);
   }
 
   const completed = async (index) => {
+    console.log(index);
     tempSectionArray[0].subsections[index].completed = true;
     const tempSectionArrayOne = tempSectionArray.slice();
     setTempSectionArray(tempSectionArrayOne);
@@ -29,57 +33,88 @@ const InsertionAccordion = ({ tempSectionArray, setTempSectionArray, updateLocal
       const temp = tempSectionArray.slice();
       setTempSectionArray(temp);
     }
-    updateLocalUser(tempSectionArray);
+    // updateLocalUser(tempSectionArray);
+    handleAccordClick(tempSectionArray[0].subsections[index].name);
+    handleClose();
   };
 
   return (
-    <Accordion
-      defaultExpanded
-      sx={{
-        backgroundColor: `${tempSectionArray[0].completed ? "#4db866" : "#ff8178"}`,
-      }}
-    >
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="h3">{tempSectionArray[0].sectionName}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        {tempSectionArray[0].subsections.map((subsection, index) => (
-          <Accordion
-            key={subsection.name}
-            expanded={currentSubSection === subsection.name}
-            disabled={index === 0 ? false : !tempSectionArray[0].subsections[index - 1].completed}
-            onClick={
-              index === 0 || tempSectionArray[0].subsections[index - 1].completed
-                ? () => handleAccordClick(subsection.name)
-                : null
-            }
-          >
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Grid container sx={{ justifyContent: "space-between", alignItems: "center" }}>
-                <Typography variant="h5">{subsection.name}</Typography>
-                {subsection.completed ? (
-                  <CheckBoxIcon color="success" fontSize="large" />
-                ) : (
-                  <CheckBoxOutlineBlankIcon fontSize="large" />
-                )}
-              </Grid>
-            </AccordionSummary>
-            <AccordionDetails>
-              {index === 0 ? <InsertionGeneral /> : index === 1 ? <InsertionCode /> : null}
-            </AccordionDetails>
-            {!subsection.completed ? (
-              <Button onClick={() => completed(index)} variant="contained">
-                Take Quiz
-              </Button>
-            ) : (
-              <Button onClick={() => handleAccordClick(index)} variant="contained">
-                Close
-              </Button>
-            )}
-          </Accordion>
-        ))}
-      </AccordionDetails>
-    </Accordion>
+    <>
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+          <Button onClick={() => completed(subsectionIndexRef.current)} variant="contained">
+            Take Quiz
+          </Button>
+          <Button onClick={() => handleClose()} variant="contained">
+            Close
+          </Button>
+        </Box>
+      </Modal>
+      <Accordion
+        defaultExpanded
+        sx={{
+          backgroundColor: `${tempSectionArray[0].completed ? "#4db866" : "#ff8178"}`,
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h3">{tempSectionArray[0].sectionName}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {tempSectionArray[0].subsections.map((subsection, index) => (
+            <Accordion
+              key={subsection.name}
+              expanded={currentSubSection === subsection.name}
+              disabled={index === 0 ? false : !tempSectionArray[0].subsections[index - 1].completed}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                onClick={
+                  index === 0 || tempSectionArray[0].subsections[index - 1].completed
+                    ? () => handleAccordClick(subsection.name)
+                    : null
+                }
+              >
+                <Grid container sx={{ justifyContent: "space-between", alignItems: "center" }}>
+                  <Typography variant="h5">{subsection.name}</Typography>
+                  {subsection.completed ? (
+                    <CheckBoxIcon color="success" fontSize="large" />
+                  ) : (
+                    <CheckBoxOutlineBlankIcon fontSize="large" />
+                  )}
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+                {index === 0 ? <InsertionGeneral /> : index === 1 ? <InsertionCode /> : null}
+              </AccordionDetails>
+              {!subsection.completed ? (
+                <Button
+                  onClick={() => {
+                    subsectionIndexRef.current = index;
+                    handleOpen();
+                  }}
+                >
+                  Open modal
+                </Button>
+              ) : (
+                <Button onClick={() => handleAccordClick(index)} variant="contained">
+                  Close
+                </Button>
+              )}
+            </Accordion>
+          ))}
+        </AccordionDetails>
+      </Accordion>
+    </>
   );
 };
 
