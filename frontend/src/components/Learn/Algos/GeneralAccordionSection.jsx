c81537c936386aea02b871584c50ab93294c4fe1
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-16 20:28:17 -0700

    created reusable general accordion section component

---
+++ frontend/src/components/Learn/Algos/GeneralAccordionSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import InsertionGeneral from "./InsertionInfo/InsertionGeneral";
+import SelectionGeneral from "./SelectionInfo/SelectionGeneral";
+import MergeGeneral from "./MergeInfo/MergeGeneral";
+import QuickGeneral from "./QuickInfo/QuickGeneral";
+
+const GeneralAccordionSection = ({ sectionNum }) => {
+  return sectionNum === 0 ? (
+    <InsertionGeneral />
+  ) : sectionNum === 1 ? (
+    <SelectionGeneral />
+  ) : sectionNum === 1 ? (
+    <MergeGeneral />
+  ) : sectionNum === 1 ? (
+    <QuickGeneral />
+  ) : null;
+};
+
+export default GeneralAccordionSection;
