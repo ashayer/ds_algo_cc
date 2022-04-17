34934fa024a9522d68e52e2bc76c615da9a9c575
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-16 23:35:06 -0700

    added css to complexity table

--- frontend/src/components/Learn/Algos/ComplexityTable.jsx
+++ frontend/src/components/Learn/Algos/ComplexityTable.jsx
@@ -12,33 +12,45 @@ import {
 const ComplexityTable = ({ timeComplexityArray, space }) => {
   return (
     <>
-      <Typography>Time Complexities</Typography>
+      <Typography variant="h5" sx={{ borderBottom: "1px solid black" }}>
+        Time Complexities
+      </Typography>
       <TableContainer>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell align="center">Best</TableCell>
-              <TableCell align="center">Average</TableCell>
-              <TableCell align="center">Worst</TableCell>
+              <TableCell align="center">
+                <Typography variant="h6">Best</Typography>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="h6">Average</Typography>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="h6">Worst</Typography>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
               {timeComplexityArray.map((time, index) => (
-                <TableCell key={index} align="center">
-                  {time}
+                <TableCell key={index} align="center" sx={{ border: 0 }}>
+                  <Typography variant="h6">{time}</Typography>
                 </TableCell>
               ))}
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
-      <Typography>Space Complexity</Typography>
+      <Typography variant="h5" sx={{ borderBottom: "1px solid black" }}>
+        Space Complexity
+      </Typography>
       <TableContainer>
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell align="center">{space}</TableCell>
+              <TableCell align="center">
+                <Typography variant="h6">{space}</Typography>
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
