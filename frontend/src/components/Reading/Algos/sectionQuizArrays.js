e7a491d5a9515902750538e98d88fcdcfa0ad9fe
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-29 19:25:51 -0700

    finalized the quick sort accordion quiz

--- frontend/src/components/Reading/Algos/sectionQuizArrays.js
+++ frontend/src/components/Reading/Algos/sectionQuizArrays.js
@@ -254,84 +254,85 @@ export const mergeCodeQuiz = [
 
 export const quickGeneralQuiz = [
   {
-    question: "QUIQUICCUQICUQICUQIQC",
+    question: "What is generally the best pivot choice?",
     type: 0,
     options: [
-      { answer: "Quick sort has a best case of n", correct: false },
-      { answer: "Quick sort is stable and in-place", correct: false },
-      { answer: "Quick sort has a best case of n\u00B2", correct: true },
-      { answer: "Quick sort is efficient on smaller datasets", correct: false },
+      { answer: "The middle index", correct: false },
+      { answer: "The average value of first, middle, and last index", correct: false },
+      { answer: "The left most index", correct: true },
+      { answer: "The median of the first, middle, and last index", correct: false },
     ],
   },
   {
-    question:
-      "What would be the output of Quick sort if we sorted the tuples by their letter. " +
-      "(C,1) (B,3) (A,2) (A,1) (B,1)",
+    question: "Which best describes the function of the pivot?",
     type: 0,
     options: [
-      { answer: "(A,1) (A,2) (B,1) (B,3) (C,1)", correct: false },
-      { answer: "(A,2) (A,1) (B,3) (B,1) (C,1)", correct: true },
-      { answer: "(A,2) (A,1) (B,1) (B,3) (C,1)", correct: false },
-      { answer: "(A,1) (B,1) (C,1) (A,2) (B,3) ", correct: false },
+      { answer: "To split the list into equal halves", correct: false },
+      { answer: "To separate elements by their value compared to the pivot", correct: true },
+      { answer: "To find the smallest value in the list", correct: false },
+      { answer: "To act as a pointer to swap elements", correct: false },
     ],
   },
   {
-    question: "Quick Sort is in-place but unstable",
+    question: "Quick Sort breaks down each element into its own sub-list",
     type: 0,
     options: [
-      { answer: "true", correct: false },
-      { answer: "false", correct: true },
+      { answer: "True", correct: false },
+      { answer: "False", correct: true },
     ],
   },
   {
-    question: "Check all that apply to Quick sort",
+    question: "With the leftmost as the pivot, which list would give the worst case performance?",
     type: 1,
     options: [
-      { answer: "Has a space complexity of O(1)", correct: true },
-      { answer: "Has a space tune of O(n\u00B2)", correct: true },
-      { answer: "Sorts a list in-place", correct: true },
-      { answer: "Will maintain the order of the original list", correct: true },
+      { answer: "[1,1,1,1,1]", correct: true },
+      { answer: "[5,4,3,2,1]", correct: true },
+      { answer: "[1,2,3,4,5]", correct: true },
+      { answer: "[2,1,4,5,3]", correct: false },
     ],
   },
 ];
 
 export const quickCodeQuiz = [
   {
-    question: "QUIQUICCUQICUQICUQIQC",
+    question: "What does the partition function return?",
     type: 0,
     options: [
-      { answer: "The elements to the left of the ith iterator are sorted", correct: true },
-      { answer: "The element to the right of the ith iterator are sorted", correct: false },
-      { answer: "The inner loop breaks if the entire array is sorted", correct: false },
-      { answer: "The outer loop starts at the first element", correct: false },
+      { answer: "The sorted array", correct: false },
+      { answer: "The next pivot index", correct: true },
+      { answer: "One half the passed in array", correct: false },
+      { answer: "A sorted array", correct: false },
     ],
   },
   {
-    question: "What are the conditions that will end the inner for loop with the J iterator",
+    question: "How is the pivot index found in the partition function?",
     type: 1,
     options: [
-      { answer: "J reaches the first element", correct: true },
-      { answer: "J becomes 0", correct: true },
-      { answer: "The list is sorted", correct: false },
-      { answer: "The value at j-1 is less than or greater than the value at j", correct: true },
+      {
+        answer: "Index where smaller values are on the left and bigger on the right",
+        correct: true,
+      },
+      { answer: "The index in the middle of the array", correct: true },
+      { answer: "The smallest value in the array", correct: false },
+      { answer: "The largest value in the array", correct: true },
     ],
   },
   {
-    question: "What is the equivalent while loop for the inner j loop of Quick sort",
+    question: "What is the state of the array after the first partition loop. [3,1,2,4]",
     type: 0,
     options: [
-      { answer: "while(j > 0)", correct: false },
-      { answer: "while(j > 1)", correct: false },
-      { answer: "while(j < i)", correct: false },
-      { answer: "while(j > -1)", correct: true },
+      { answer: "[2,1,4,3]", correct: false },
+      { answer: "[1,2,4,3]", correct: false },
+      { answer: "[2,1,3,4]", correct: true },
+      { answer: "[1,2,3,4]", correct: false },
     ],
   },
   {
-    question: "The jth and jth-1 values are swapped in the algorithm",
+    question: "The pivotIndex is swapped with the leftmost index",
     type: 0,
     options: [
-      { answer: "true", correct: true },
-      { answer: "false", correct: false },
+      { answer: "True", correct: true },
+      { answer: "False", correct: false },
     ],
   },
 ];
