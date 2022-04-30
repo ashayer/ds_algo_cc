22212b6af855e59a0c4166f51fff4f1e7be39a8e
Aran Hayer <ashayer@users.noreply.github.com>
2022-04-29 18:17:42 -0700

    finalized merge sort accordion quiz

--- frontend/src/components/Reading/Algos/sectionQuizArrays.js
+++ frontend/src/components/Reading/Algos/sectionQuizArrays.js
@@ -3,10 +3,10 @@ export const insertionGeneralQuiz = [
     question: "Which of the following is NOT true about Insertion Sort",
     type: 0,
     options: [
-      { answer: "Insertion sort has an average time complexity of n\u00B2", correct: false },
-      { answer: "Insertion sort is stable and in-place", correct: false },
-      { answer: "Insertion sort has a best case of n", correct: false },
-      { answer: "Insertion sort is efficient on all datasets", correct: true },
+      { answer: "Has an average time complexity of n\u00B2", correct: false },
+      { answer: "Stable and in-place", correct: false },
+      { answer: "Best case of n", correct: false },
+      { answer: "Efficient on all datasets", correct: true },
     ],
   },
   {
@@ -117,10 +117,10 @@ export const selectionGeneralQuiz = [
     question: "Which of the following is true about Selection Sort",
     type: 0,
     options: [
-      { answer: "Selection sort has a best case of n\u00B2", correct: true },
-      { answer: "Selection sort has a best case of n", correct: false },
-      { answer: "Selection sort is stable and in-place", correct: false },
-      { answer: "Selection sort swaps the ith and jth indexes", correct: false },
+      { answer: "Has a best case of n\u00B2", correct: true },
+      { answer: "Has a best case of n", correct: false },
+      { answer: "Is stable and in-place", correct: false },
+      { answer: "Swaps the ith and jth indexes", correct: false },
     ],
   },
 ];
@@ -166,16 +166,6 @@ export const selectionCodeQuiz = [
 
 export const mergeGeneralQuiz = [
   {
-    question: "MERGEMRMGERMGEMERMGE",
-    type: 0,
-    options: [
-      { answer: "Merge sort has a best case of n", correct: false },
-      { answer: "Merge sort is stable and in-place", correct: false },
-      { answer: "Merge sort has a best case of n\u00B2", correct: true },
-      { answer: "Merge sort is efficient on smaller datasets", correct: false },
-    ],
-  },
-  {
     question:
       "What would be the output of Merge sort if we sorted the tuples by their letter. " +
       "(C,1) (B,3) (A,2) (A,1) (B,1)",
@@ -187,63 +177,77 @@ export const mergeGeneralQuiz = [
       { answer: "(A,1) (B,1) (C,1) (A,2) (B,3) ", correct: false },
     ],
   },
+
   {
-    question: "Merge Sort is in-place but unstable",
+    question: "Merge Sort splits the list into two halves",
     type: 0,
     options: [
-      { answer: "true", correct: false },
-      { answer: "false", correct: true },
+      { answer: "True", correct: true },
+      { answer: "False", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is true about Merge Sort?",
+    type: 0,
+    options: [
+      {
+        answer: "Its space complexity is constant",
+        correct: false,
+      },
+      { answer: "Has a worst time complexity of n\u00B2", correct: false },
+      { answer: "It is stable but out-of-place", correct: true },
+      { answer: "Its worst case is slower than its best case", correct: false },
     ],
   },
   {
     question: "Check all that apply to Merge sort",
     type: 1,
     options: [
-      { answer: "Has a space complexity of O(1)", correct: true },
-      { answer: "Has a space tune of O(n\u00B2)", correct: true },
-      { answer: "Sorts a list in-place", correct: true },
       { answer: "Will maintain the order of the original list", correct: true },
+      { answer: "Quadratic", correct: false },
+      { answer: "Requires extra space proportional to size of the list", correct: true },
+      { answer: "Utilizes divide and conquer", correct: true },
     ],
   },
 ];
 
 export const mergeCodeQuiz = [
   {
-    question: "MERGEMRMGERMGEMERMGE",
+    question: "What condition stops the recursive calls?",
     type: 0,
     options: [
-      { answer: "The elements to the left of the ith iterator are sorted", correct: true },
-      { answer: "The element to the right of the ith iterator are sorted", correct: false },
-      { answer: "The inner loop breaks if the entire array is sorted", correct: false },
-      { answer: "The outer loop starts at the first element", correct: false },
+      { answer: "The right index is less than or equal to the left index", correct: true },
+      { answer: "The left index is less than the middle index", correct: false },
+      { answer: "The left index is less than the right index", correct: false },
+      { answer: "The right index is greater than the middle index", correct: false },
     ],
   },
   {
-    question: "What are the conditions that will end the inner for loop with the J iterator",
+    question: "What are the parameters passed into the MERGE function?",
     type: 1,
     options: [
-      { answer: "J reaches the first element", correct: true },
-      { answer: "J becomes 0", correct: true },
-      { answer: "The list is sorted", correct: false },
-      { answer: "The value at j-1 is less than or greater than the value at j", correct: true },
+      { answer: "An array", correct: true },
+      { answer: "A left index", correct: true },
+      { answer: "A right index", correct: true },
+      { answer: "A middle index ", correct: true },
     ],
   },
   {
-    question: "What is the equivalent while loop for the inner j loop of Merge sort",
+    question: "In the MERGE function what is i and j initialized to.",
     type: 0,
     options: [
-      { answer: "while(j > 0)", correct: false },
-      { answer: "while(j > 1)", correct: false },
-      { answer: "while(j < i)", correct: false },
-      { answer: "while(j > -1)", correct: true },
+      { answer: "i = 0 j = right index,", correct: false },
+      { answer: "i = left j = middle", correct: false },
+      { answer: "i = left + 1 j = middle", correct: false },
+      { answer: "i = left j = middle + 1", correct: true },
     ],
   },
   {
-    question: "The jth and jth-1 values are swapped in the algorithm",
+    question: "The created temp array is updated with values from the parameter array",
     type: 0,
     options: [
-      { answer: "true", correct: true },
-      { answer: "false", correct: false },
+      { answer: "True", correct: false },
+      { answer: "False", correct: true },
     ],
   },
 ];
