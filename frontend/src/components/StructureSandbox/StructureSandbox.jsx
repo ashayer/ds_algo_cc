64156e9bd89a4e334f3fc027cf2d953e8ccc8274
Aran Hayer <ashayer@users.noreply.github.com>
2022-05-05 01:16:40 -0700

    created template for structure tool bars in sandbox

--- frontend/src/components/StructureSandbox/StructureSandbox.jsx
+++ frontend/src/components/StructureSandbox/StructureSandbox.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
+import StructureOptions from "./StructureOptions";
+import QueueDisplay from "./Structures/QueueDisplay";
+import StackDisplay from "./Structures/StackDisplay";
+import LinkedListDisplay from "./Structures/LinkedListDisplay";
+import BinaryTreeDisplay from "./Structures/BinaryTreeDisplay";
 
 const SortingSandbox = () => {
-  // const handleAlgoChange = (e) => {
-  //   console.log(e.target.value);
-  // };
+  const [dataStructure, setDataStructures] = useState(0);
+
+  const handleStructureChange = (e) => {
+    setDataStructures(e.target.value);
+  };
 
   return (
     <>
       <Navbar page="Data Structure Sandbox" />
-
-      <Grid container>
-        <Grid item>Data</Grid>
-      </Grid>
+      <StructureOptions
+        handleStructureChange={handleStructureChange}
+        dataStructure={dataStructure}
+      />
+      {dataStructure === 0 ? (
+        <QueueDisplay />
+      ) : dataStructure === 1 ? (
+        <StackDisplay />
+      ) : dataStructure === 2 ? (
+        <LinkedListDisplay />
+      ) : dataStructure === 3 ? (
+        <BinaryTreeDisplay />
+      ) : null}
     </>
   );
 };
---
+++ frontend/src/components/StructureSandbox/Structures/LinkedListDisplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const LinkedListDisplay = () => {
+  return <Grid item>List</Grid>;
+};
+
+export default LinkedListDisplay;
